386067e75cfdee65cea7e5a296f1eeac3d7e710c
Ravy <ravieli.bomfim@gmail.com>
2023-10-11 16:05:17 -0300

    Criar Perfil automaticamente ao criar Usuário

--- paginas/views.py
+++ paginas/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    login_url = reverse_lazy('login')
     template_name = "paginas/index.html"
 
     def get(self, request, *args, **kwargs):
