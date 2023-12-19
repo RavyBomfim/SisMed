52b58f8f3e122c7157a5c46caec157ee2440382f
Ravy <ravieli.bomfim@gmail.com>
2023-12-19 12:19:50 -0300

    Últimas alterações

--- SisMed/settings.py
+++ SisMed/settings.py
@@ -42,13 +42,7 @@ INSTALLED_APPS = [
     'cadastros.apps.CadastrosConfig',
     'usuarios.apps.UsuariosConfig',
     'relatorios.apps.RelatoriosConfig',
-    'crispy_forms',
-    'crispy_bootstrap5',
 ]
-
-# Crispy Forms
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
